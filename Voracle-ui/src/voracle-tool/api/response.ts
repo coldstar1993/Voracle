c82af55b561c02f0d35bf5e3b136dad71d444ea1
coldstar1993 <coldstar1993@163.com>
2022-12-17 15:28:37 +0800

    pick key component from voracle-tool

---
+++ Voracle-ui/src/voracle-tool/api/response.ts
@@ -0,0 +1,46 @@
+
+import { BlockSummary } from "./block_entity";
+export interface AccountResonse {
+  fetcherPk: string,
+  pkIdx: string,
+  asset: string,
+  free: string,
+  locked: string,
+  fetchSig: [string, string],
+}
+
+export interface AccountResonseList {
+  data: AccountResonse[]
+}
+
+
+export interface BlockSummaryResponse {
+    fetcherPk: string,
+    pkIdx: string,
+    data: BlockSummary
+    fetchSig: [string, string],
+}
+export interface BlockSummaryResponseList {
+    data: BlockSummaryResponse[]
+}
+
+export interface NetworkSupplyStatusResponse {
+    fetcherPk: string,
+    pkIdx: string,
+    data: { blockchainLength: number, circulatingSupply: string, lockedSupply: string },
+    fetchSig: [string, string],
+}
+export interface NetworkSupplyStatusResponseList {
+  data: NetworkSupplyStatusResponse[]
+}
+
+export interface CommonResponse {
+    fetcherPk: string,
+    pkIdx: string,
+    data: any,
+    fetchSig: [string, string],
+}
+export interface CommonResponseList {
+    data: CommonResponse[]
+}
+
